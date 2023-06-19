3f8851b4e1f8315218b1cf8cfcf0805757a5892f
Nicolas-sapriza <nsaprizabrignoni+github@gmail.com>
2023-06-19 11:58:33 -0300

    Fix docs, add clave_de_relacion_universal, add all remaining table to db_setup

--- src/db_setup.sql
+++ src/db_setup.sql
@@ -80,11 +80,108 @@ CREATE TABLE Ventanilla(
 CREATE TABLE Debito(
     debito_id                   INT         SERIAL      PRIMARY KEY,
     debito_vehiculo_matricula   VARCHAR(7)  NOT NULL,
+    debioto_peaje_id            INT         NOT NULL
     debito_ventanilla_numero    INT         NOT NULL, 
     debito_fecha_y_hora         DATETIME    NOT NULL,
-    debito_cuenta_numero        INT         NOT NULL,
+    debito_cuenta_numero        INT         NOT NULL
 );
 
+
+CREATE TABLE Bonificacion(
+    bonificacion_id                 INT             SERIAL      PRIMARY KEY,
+    bonificacion_cuenta_numero      INT             NOT NULL, 
+    bonificacion_fecha_otorgo       DATE            NOT NULL, 
+    bonificacion_descuento          SMALLINT        NOT NULL, 
+    bonificacion_motivo             VARCHAR(100)    NOT NULL,
+    bonificacion_peaje_id           INT             NOT NULL, 
+    bonificacion_fecha_renovacion   DATE            NOT NULL
+);
+
+--fks
+CREATE TABLE PropietarioPersona(
+    propietario_persona_id  INT     NOT NULL,
+    persona_id              INT     NOT NULL
+);
+
+CREATE TABLE PropietarioEmpresa(
+    propietario_empresa_id  INT     NOT NULL,
+    empresa_id              INT     NOT NULL
+);
+
+CREATE TABLE CuentaPropietario(
+    cuenta_propietario_id   INT     NOT NULL,
+    propietario_id          INT     NOT NULL
+);
+
+CREATE TABLE PropietarioVehiculo(
+    prop_veh_id_prop    INT     NOT NULL,
+    propietario_id      INT     NOT NULL
+);
+
+CREATE TABLE PropietarioVehiculo(
+    prop_veh_id  INT     NOT NULL,
+    vehiculo_id         INT     NOT NULL
+);
+
+CREATE TABLE CargaCuenta(
+    carga_numero_cuenta INT     NOT NULL,
+    cuenta_numero       INT     NOT NULL
+);
+
+CREATE TABLE vehiculoTipoVehiculo(
+    vehiculo_tipo         VARCHAR(35)     NOT NULL,
+    tipo_vehiculo_tipo    VARCHAR(35)     NOT NULL
+);
+
+CREATE TABLE TarifaTipoVehiculo(
+    tarifa_tipo_vehiculo_tipo   VARCHAR(35)     NOT NULL,
+    tipo_vehiculo_tipo          VARCHAR(35)     NOT NULL
+);
+
+CREATE TABLE VentanillaPeaje(
+    ventanilla_peaje_id     INT     NOT NULL,
+    peaje_id                INT     NOT NULL    
+);
+
+CREATE TABLE DebitoCuenta(
+    debito_cuenta_numero    INT     NOT NULL,
+    cuenta_numero           INT     NOT NULL
+);
+
+CREATE TABLE DebitoVehiculo(
+    debito_vehiculo_id  INT     NOT NULL,
+    vehiculo_id         INT     NOT NULL
+);
+
+CREATE TABLE DebitoVentanilla(
+    debito_peaje_id             INT     NOT NULL,
+    debito_ventanilla_numero    INT     NOT NULL,
+    ventanilla_numero           INT     NOT NULL
+);
+
+CREATE TABLE BonificacionCuenta(
+    bonificacion_cuenta_numero  INT     NOT NULL,
+    cuenta_numero               INT     NOT NULL
+);
+
+CREATE TABLE BonificacionPeaje(
+    bonificacion_peaje_id   INT     NOT NULL,
+    peaje_id            INT     NOT NULL
+);
+CREATE TABLE ClaveR(
+    telefono_id          INT     NOT NULL,
+    propietario_id       INT     NOT NULL,
+    cuenta_numero        INT     NOT NULL,
+    carga_id             INT     NOT NULL,
+    vehiculo_id          INT     NOT NULL,
+    prop_veh_id_prop     INT     NOT NULL,
+    prop_veh_id   INT     NOT NULL,
+    tarifa_id            INT     NOT NULL,
+    peaje_id             INT     NOT NULL,
+    ventanilla_numero    INT     NOT NULL,
+    debito_id            INT     NOT NULL,
+    bonificacion_id      INT     NOT NULL,
+);
 
 --fuentes:
 --    https://webarchive.nationalarchives.gov.uk/ukgwa/+/http://www.cabinetoffice.gov.uk/media/254290/GDS%20Catalogue%20Vol%202.pdf                                    
