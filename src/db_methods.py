b41c1b3e63f8aba89a36a1a0538bca7e2966733d
Nicolas-sapriza <nsaprizabrignoni+github@gmail.com>
2023-06-19 19:47:21 -0300

    Fix agregar_persona in db_methods

--- src/db_methods.py
+++ src/db_methods.py
@@ -1,35 +1,71 @@
-from peeweeClasses import *
+from database import postgres_database
+from peewee import *
+
+from models.basemodel import *
+from models.bonificacion import *
+from models.carga import *
+from models.cuenta import *
+from models.debito import *
+from models.empresa import *
+from models.peaje import *
+from models.persona import *
+from models.propietario import *
+from models.tarifa import *
+from models.telefono import *
+from models.tipovehiculo import *
+from models.vehiculo import *
+from models.vehiculo import *
+from models.ventanilla import *
 
 def agregar_persona():
 
     dni:str             = input("ingrese dni (sin puntos ni guion): ")
     nombre_1:str        = input("ingrese primer nombre: ")
     nombre_2:str        = input("ingrese segundo nombre: ")
-    direccion:str       = input("ingrese direccion: ")
+    direccion:str       = input("ingrese dirección: ")
     apellido_1:str      = input("ingrese primer apellido: ")
     apellido_2:str      = input("ingrese segundo apellido: ")
     mail:str            = input("ingrese e-mail: ")
     celular:str         = input("ingrese celular: ")
-    
-    asociad0:str   = input("ingrese dni de persona asociada")
+
+    hay_persona_asociada:str   = input("tiene personas asociadas (y/n): ")
+    hay_persona_asociada       = hay_persona_asociada.lower()
+    asociado = None
+
+    if(hay_persona_asociada in ["y","yes","y/","yes/","si","si/"]):
+        tmp_dni:str   = input("ingrese el dni de la persona asociada: ")
+        try:
+            temp_persona  = Persona.get(Persona.persona_dni==tmp_dni)
+            asociado      = temp_persona.persona_id
+        except DoesNotExist:
+            input("No existe una persona con ese dni, presione enter para continuar...")
+            return
+        
+    elif(hay_persona_asociada not in ["n","no","n/","no/"]):
+        input("entrada no valida, toque cualquier tecla para continuar...")
+        return
+
     try:
         new_persona = Persona.create(
-            persona_dni      =dni,
-            persona_nombre_1  =nombre_1,
-            persona_nombre_2=nombre_2,
-            persona_direccion=direccion,
-            persona_apellido_1=apellido_1,
-            persona_apellido_2=apellido_2,
-            persona_mail=mail,
-            persona_celular =    celular,
-            persona_dni_asociado=1,)
+            persona_dni         = dni,
+            persona_nombre_1    = nombre_1,
+            persona_nombre_2    = nombre_2,
+            persona_direccion   = direccion,
+            persona_apellido_1  = apellido_1,
+            persona_apellido_2  = apellido_2,
+            persona_mail        = mail,
+            persona_celular     = celular,
+            persona_dni_asociado= asociado,
+            )
     except Exception:
-        input("dato no valido ingresado para algún dato pedido")
+        input("dato no valido ingresado para algún dato pedido, toque enter para continuar...")
         return
+    else:
+        input("transacción realizada exitosamente, toque enter para continuar...")
     
 def agregar_empresa():
-    rut = input("Ingrese RUT (sin puntos ni guion): ")
-    nombre = input("Ingrese nombre: ")
+    rut:str = input("Ingrese RUT (sin puntos ni guion): ")
+    nombre:str = input("Ingrese nombre: ")
     direccion = input("Ingrese direccion: ")
 
     try:
