1431c47a443850ad91ceab146eda773035337924
giannilabella <gianni0labella@gmail.com>
2023-06-20 08:31:07 -0300

    fixup! Update models and queries

--- src/models/cuenta.py
+++ src/models/cuenta.py
@@ -4,7 +4,7 @@ from src.models.propietario import Propietario
 
 
 class Cuenta(BaseModel):
-    cuenta_numero = AutoField(primary_key=True)
+    cuenta_id = AutoField(primary_key=True)
     cuenta_saldo = IntegerField(null=False)
     cuenta_creacion = DateField(null=False)
     cuenta_propietario = ForeignKeyField(Propietario, on_delete='CASCADE', unique=True)
